b729d2cf9db0f019627caec5b23d6f8b8c633131
JuanValentin98 <skate.juan10@gmail.com>
2018-05-19 13:27:41 -0500

    Proyecto

---
+++ logica/database.php
@@ -0,0 +1,72 @@
+<?php
+
+class database {
+
+  private $pdo;//variable para iniciar el pdo
+  var $HOST = "localhost";//nombre del host que se ocupara
+  var $BDNAME = "asuncion";//nombre de la base de datos
+    var $USUARIO = "root";//nombre del usuario
+  var $PASSWORD = "";//la contraseña para acceder a la base
+
+  public function __construct() {
+    try {
+      //se conecta a la base
+      $this->pdo = new PDO('mysql:host=' . $this->HOST . ';dbname=' . $this->BDNAME, $this->USUARIO, $this->PASSWORD);//prepara la conexión
+      $this->pdo->exec("set character set utf8");
+    } catch (PDOException $e) {
+      echo 'ERROR: ' . $e->getMessage();//mensaje por si surge un error
+    }
+  }
+//cierra la conexion
+  function CerrarConexion() {
+    $this->pdo = null;
+  }
+
+  function servicio(){
+    //prepara la consulta de servicios
+    $sql = $this->pdo->prepare("select * from servicios");
+    if ($sql->execute(array(1))) {
+      //retorna los datos obtenidos de la base
+      return $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+    $this->CerrarConexion();
+  }
+
+  function bolsa(){
+    //prepara la consulta de bolsa de trabajo
+    $sql = $this->pdo->prepare("select * from bolsa_trabajo");
+    if ($sql->execute(array(1))) {
+      //retorna los datos obtenidos de la base
+      return $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+    $this->CerrarConexion();
+  }
+
+
+
+
+  function afilidao_id($user, $contraseña){
+    $sql = $this->pdo->prepare("SELECT * FROM `administrador` WHERE `Nombre_usuario` = ? AND `Password`= ?");
+    if ($sql->execute(array($user, $contraseña))) {
+      //retorna los datos obtenidos de la base
+      return $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+  }
+
+  function N_bolsa($id, $nombre, $contenido, $telefono,$fecha_limite){
+    //inserta una nueva afiliacion
+    $sql = $this->pdo->prepare("INSERT INTO bolsa_trabajo(`Id_afiliado`,`Nombre_vacante`, `Contenido`, `Telefono`,  `Fecha_limite`) VALUES ('{$id}','{$_POST['vacante']}','{$_POST['contenido']}','{$_POST['telefono']},'{$_POST['fecha']}')");
+    $sql->execute(array($id, $nombre,$contenido, $telefono,$fecha_limite));
+  }
+
+
+  function Empleado ($id, $nombre, $telefono, $email , $mensaje){
+    //inserta una nueva afiliacion
+    $sql = $this->pdo->prepare("INSERT INTO `bolsa_aspirante`(`Id_bolsa`, `Nombre`, `Telefono`, `Correo`, `Mensaje`) VALUES ('{$_POST['Id_bolsa']}','{$_POST['nombre']}','{$_POST['telefono']}','{$_POST['email']}
+      ','{$_POST['comentario']}')");
+    $sql->execute(array($id, $nombre, $telefono, $email , $mensaje));
+  }
+
+
+
+}
---
+++ servicios.php
@@ -0,0 +1,210 @@
+<?php
+include_once 'logica/funciones.php';
+$fun = new funciones();
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content=""> 
+  <meta name="author" content="">
+
+  <title>Confecciones La Asunción S.A DE C.V</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link href="css/business-casual.css" rel="stylesheet">
+  <link href="css/Style.css" rel="stylesheet">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+  <link href="https://fortawesome.github.io/Font-Awesome/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  
+  <!--  codigo de play sin botones :  <audio src="music/lilly wood" autoplay="true" loop="true"></audio> -->
+</head>
+
+<body>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <?php $fun->menu();  ?>
+
+
+  <div class="container">
+
+    <div class="row">
+      <div class="box">
+        <div class="col-lg-12 text-center">
+
+            <h1 class="brand-name" style="color: #000; font-size: 50px ">S E R V I C I O S</h1>
+
+        
+      </div>
+    </div>
+  </div>
+
+      
+      
+        <div class="row">
+    <div class="box">
+
+        
+            <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Nuestros servicios</h2>
+            <hr class="my-4">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i>
+              <h3 class="mb-3">DISEÑO Y PATRONAJE</h3>
+              <p class="text-muted mb-0">-Patrocinaje. <br><br> -Fichas Tecnicas. <br><br> -Elaboracion de Muestras.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
+              <h3 class="mb-3">GRADUACION</h3>
+              <p class="text-muted mb-0">-Creacion. <br><br> -Copiado y/o Modificacion de Reglas de Graduacion.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fa fa-4x fa-newspaper-o text-primary mb-3 sr-icons"></i>
+              <h3 class="mb-3">TRAZO</h3>
+              <p class="text-muted mb-0">-Acomodos opticos. <br><br> -Calculos de Consumos. <br><br> -Impresiones de moldes y trazos.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i>
+              <h3 class="mb-3">CORTE Y FOLEO</h3>
+              <p class="text-muted mb-0">-Servicio de Corte Automatico. <br><br> -Fudionado y Foleo para todo tipo de prenda.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+        
+        
+        
+    </div>
+  </div>
+      
+      
+      
+  <div class="row">
+    <div class="box">
+      <div class="col-lg-12">
+        <hr>
+        <h2 class="intro-text text-center">
+          <a href=""><strong><li><a href="index.php">DISEÑO Y PATRONAJE</a></li></strong></a>
+        </h2>
+        <hr>
+        <img class="img-responsive img-border img-left" src="img/serv.jpg" alt="">
+        <hr class="visible-xs">
+        <br><br> <p>-Patronaje. <br><br> -Fichas Tecnicas. <br><br> -Elaboracion de Muestras.</p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="box">
+      <div class="col-lg-12">
+        <hr>
+        <h2 class="intro-text text-center">
+          <a href=""><strong><a href="index.php">GRADUACION</a></li></strong></a>
+        </h2>
+        <hr>
+        <img class="img-responsive img-border img-left" src="img/cur.jpg" alt="">
+        <hr class="visible-xs">
+        <br><br><br> <p>-Creacion. <br><br> -Copiado y/o Modificacion de Reglas de Graduacion.</p>
+
+
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="box">
+      <div class="col-lg-12">
+        <hr>
+        <h2 class="intro-text text-center">
+          <a href=""><strong><a href="index.php">TRAZO</a></li></strong></a>
+        </h2>
+        <hr>
+        <img class="img-responsive img-border img-left" src="img/oportunidad.jpg" alt="">
+        <hr class="visible-xs">
+       <br><br> <p>-Acomodos opticos. <br><br> -Calculos de Consumos. <br><br> -Impresiones de moldes y trazos.</p>
+      </div>
+    </div>
+  </div>
+
+        <div class="row">
+    <div class="box">
+      <div class="col-lg-12">
+        <hr>
+        <h2 class="intro-text text-center">
+          <a href=""><strong><a href="index.php">CORTE Y FOLEO</a></li></strong></a>
+        </h2>
+        <hr>
+        <img class="img-responsive img-border img-left" src="img/cur.jpg" alt="">
+        <hr class="visible-xs">
+       <br><br><br> <p>-Servicio de Corte Automatico. <br><br> -Fudionado y Foleo para todo tipo de prenda.</p>
+
+
+      </div>
+    </div>
+  </div>
+      
+        <div class="row">
+    <div class="box">
+      <div class="col-lg-12">
+        <hr>
+        <h2 class="intro-text text-center">
+          <a href=""><strong><a href="index.php">FUSIONADO</a></li></strong></a>
+        </h2>
+        <hr>
+        <img class="img-responsive img-border img-left" src="img/serv.jpg" alt="">
+        <hr class="visible-xs">
+       <br><br><br> <p>-Fusionado en bloque. <br><br> -Fusionado por pieza.</p>
+
+
+      </div>
+    </div>
+  </div>
+
+
+</div>
+<!-- /.container -->
+</body>
+<!--footer start from here-->
+
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/modals.js"></script>
+
+
+
+  <?php $fun->footer();  ?>
+ <?php $fun->music();  ?>
+
+</html>
